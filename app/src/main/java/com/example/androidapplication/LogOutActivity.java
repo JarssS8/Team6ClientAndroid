1546bdb499d51fa12cfa761dd9ed066feca55e03
jarsss8 <adriancgs@gmail.com>
2019-10-21 13:30:14 +0200

    First commit on Android

---
+++ app/src/main/java/com/example/androidapplication/LogOutActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogOutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_out_activity);
+    }
+}
